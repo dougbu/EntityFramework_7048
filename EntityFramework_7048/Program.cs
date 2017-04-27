989eb532010009004b7be3d81d0b3ed470ed2aba
Doug Bunting <dougbu@microsoft.com>
2017-04-27 15:11:58 -0700

    Initial commit

---
+++ EntityFramework_7048/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework_7048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var context = new MyDbContext())
+            {
+                context.Database.EnsureCreated();
+
+                var suppliers = context.Suppliers
+                    .OrderBy(x => x.Name)
+                    .Select(x => new
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString(),
+                    })
+                    .ToList();
+
+                Console.WriteLine(suppliers.Count);
+            }
+        }
+    }
+
+    public class Supplier
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-EntityFramework_7048;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
