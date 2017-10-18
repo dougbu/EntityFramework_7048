e4a3f96fdc43ed398d25365f5ed90dc8ec5dd6e8
Doug Bunting <dougbu@microsoft.com>
2017-10-18 12:27:50 -0700

    Switch to SQL Server and add aspnet/EntityFrameworkCore#9277 / aspnet/EntityFrameworkCore#9309 scenarios

--- EntityFramework_7048/Program.cs
+++ EntityFramework_7048/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,7 +15,22 @@ namespace EntityFramework_7048
 
             using (var context = new MyDbContext())
             {
+                context.Database.EnsureDeleted();
                 context.Database.EnsureCreated();
+
+                context.Database.ExecuteSqlCommand(
+                    @"CREATE PROCEDURE [dbo].[GetSuppliersCount]
+(
+    @Value int OUTPUT
+)
+AS
+BEGIN
+    SELECT @Value = Count(*)
+    FROM [dbo].[Suppliers];
+    SELECT *
+    FROM [dbo].[Suppliers]
+END");
+                context.SaveChanges();
 
                 var match = new Supplier
                 {
@@ -34,29 +51,55 @@ namespace EntityFramework_7048
                     .ToList();
                 Console.WriteLine($"Aggregated supplier count: {suppliers.Count}.");
 
+                // See aspnet/EntityFrameworkCore#9277 and aspnet/EntityFrameworkCore#9309
+                var parameter = new SqlParameter
+                {
+                    ParameterName = "Value",
+                    Direction = ParameterDirection.Output,
+                    SqlDbType = SqlDbType.Int,
+                    Value = -1,
+                };
+                context.Database.ExecuteSqlCommand("SELECT @Value = Count(*) FROM [dbo].[Suppliers]", parameter);
+                Console.WriteLine($"Direct supplier count: {parameter.Value}.");
+                parameter.Value = -1;
+
+                context.Database.ExecuteSqlCommand("[dbo].[GetSuppliersCount] @Value out", parameter);
+                Console.WriteLine($"Procedure supplier count: {parameter.Value}.");
+                parameter.Value = -1;
+
+                context.Suppliers.FromSql("[dbo].[GetSuppliersCount] @Value out", parameter).ToList();
+                Console.WriteLine($"FromSql supplier count: {parameter.Value}.");
+                parameter.Value = -1;
+
+                context.Database.ExecuteSqlCommand("[dbo].[GetSuppliersCount] @Value out", parameter);
+                Console.WriteLine($"Procedure supplier count: {parameter.Value}.");
+                parameter.Value = -1;
+
+                context.Suppliers.FromSql("[dbo].[GetSuppliersCount] @Value out", parameter).ToList();
+                Console.WriteLine($"FromSql supplier count: {parameter.Value}.");
             }
         }
-    }
 
-    public class Supplier
-    {
-        [Key]
-        public Guid Id { get; set; }
-
-        [Required]
-        public string Name { get; set; }
-
-        [EmailAddress]
-        public string Email { get; set; }
-    }
-
-    public class MyDbContext : DbContext
-    {
-        public DbSet<Supplier> Suppliers { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        private class Supplier
         {
-            optionsBuilder.UseSqlite("Data Source=EntityFramework_7048.db");
+            [Key]
+            public Guid Id { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+
+            [EmailAddress]
+            public string Email { get; set; }
+        }
+
+        private class MyDbContext : DbContext
+        {
+            public DbSet<Supplier> Suppliers { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EF_7048;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
         }
     }
 }
